13faeda7d02b8ff587c371870a88f6e1ba1bd6a9
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2021-10-22 12:13:07 -0300

    Files Added

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# brandbil
-Brandbil C Code
+# Brandbil
+
+![Alt Text](assets/brandbil.gif)
+
+In recent years, the presence of fires worldwide has been increasing. Not only were human lives taken, the economic cost behind the fires is very high. The solution? Brandil:
+
+![Alt Text](assets/brandbil.png)
+
+This is a electronic technician proyect to build a little machine that detects and prevents fires. It's just a propotype. The idea is to build something bigger. This is the picture of the prototype:
+
+![Alt Text](assets/realBrandbil.jpg)
+
+We use 16F886 PIC Microcontroller and C/Assembly programming. If you want more information, we have the technical and general spanish informs in the repository.
+
+# Developers
+<ul>
+    <li>Martin Hernan Mollo</li>
+    <li>Nahuel Gonzalo Liberto</li>
+    <li>Lucas Ariel Saavedra</li>
+</ul>
---
+++ src/brandbil.c
@@ -0,0 +1,487 @@
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#:,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#@#':::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#''::;,#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,   #@@@@    @@@@@@@@@@@@@#++;;,:,;@@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@           `,#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,   #@@@@    @@@@@@@@@@@@@+#'::,,,;@@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@               :@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,   #@@@@    @@@@@@@@@@@@@@':::::,'+@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@                 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,   #@@@@    @@@@@@@@@@@@@@+':,:::''#@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@                 ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,   #@@@@    @@@@@@@@@@@@@#';,,::;++@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,   #@@@@    @@@@@@@@@@@@+'+;,,'':'@@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,   #@@@@    @@@@@@@@@@@@@@+:::,.::@@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@    @@@@`   @@@,  ;@@@@,       '@@@@@@@    @@@      .@@@@@@@@@@:      ;@@,   #@@@@    @@@      .@@#@@,;:,,,'@@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@    @@@@`   @@    ;@@            @@@@@@    @.         @@@@@@@@          @,   #@@@@    @,         :@@@@,:,::,#@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@    @@@@`   @     ;@              @@@@@    ,           @@@@@#            ,   #@@@@    ,           .@@@@+.;:;#@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@#    @@@@@`   `    .'@@   +@@@@.    ;@@@@        ,,      .@@@@     `#@@+       #@@@@       ;@@@;     +@@@':,,,+@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@                #@@@@@`     .@@@@@@ @@@@@@@@'    @@@@      @@@@@@     @@@`    @@@@@@@'     #@@@@      @@@@@@@     @@#'::,:@@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@              ;@@@@@@@`    :@@@@@@@@@@@@@@@@@    @@@@     @@@@@@@@    @@@    #@@@@@@@@.    #@@@@     @@@@@@@@@    #@#;,',;;@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@                ,@@@@@`    @@@@@@@@@@@@@@@@@@    @@@@    #@@@@@@@@    @@#    @@@@@@@@@@    #@@@@    '@@@@@@@@@;   .@#;,:::+@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    ```````       @@@@`   '@@@@@@@@@@,           @@@@    @@@@@@@@@`   @@:   :@@@@@@@@@@    #@@@@    @@@@@@@@@@@  ,;@+;,,::+#@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@'    ;@@@`   @@@@@@@@@              @@@@    @@@@@@@@@`   @@.   #@@@@@@@@@@.   #@@@@    @@@@@@@@@@@  ';@@'::::+@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@#    @@@`   @@@@@@@@               @@@@    @@@@@@@@@`   @@.   #@@@@@@@@@@.   #@@@@    @@@@@@@@@@@ :'';;::;::@@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@@    @@@`   @@@@@@@    .@@@@@@@    @@@@    @@@@@@@@@`   @@;   '@@@@@@@@@@    #@@@@    @@@@@@@@@@@ ':''''::::;@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@@    @@@`   @@@@@@@    @@@@@@@@    @@@@    @@@@@@@@@`   @@#    @@@@@@@@@@    #@@@@    '@@@@@@@@@; ;`''';,;:'''@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@@    @@@`   @@@@@@@   ,@@@@@@@@    @@@@    @@@@@@@@@`   @@@    @@@@@@@@@;    #@@@@     @@@@@@@@@   ''';;:;:'''@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    @@@@@@@@@@     @@@`   @@@@@@@    @@@@@@@.    @@@@    @@@@@@@@@`   @@@`    @@@@@@@@     #@@@@      @@@@@@@    ;#@'':;;;+'@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@    ;;;;;;;.      ;@@@`   @@@@@@@    ,@@@@@      @@@@    @@@@@@@@@`   @@@@     :@@@@.      #@@@@       '@@@'     :@@@':;::++@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@                  @@@@`   @@@@@@@#               @@@@    @@@@@@@@@`   @@@@+            ,   #@@@@    .            @';+',::;;@@@ `  @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@                .@@@@@`   @@@@@@@@,         .    @@@@    @@@@@@@@@`   @@@@@#          @,   #@@@@    @`         `@@+;';,::;'+@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@              :@@@@@@@`   @@@@@@@@@@       @@    @@@@    @@@@@@@@@`   @@@@@@@,      ,@@,   #@@@@    @@#       @@@@@'+:.:,;'@@@    @@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@::,,,,:++@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+++';';;'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Desarroladores:
+
+-Martin Hernan Mollo
+-Nahuel Gonzalo Liberto
+-Lucas Ariel Saavedra
+------------------------------------------------------------------------------------------------------
+Año: 2017
+------------------------------------------------------------------------------------------------------
+Lugar: Argentina
+------------------------------------------------------------------------------------------------------
+Fecha de entrega: 10/2017
+------------------------------------------------------------------------------------------------------
+Programa:    Extintor de fuego automatico. "Brandbil" en suizo significa camion de bomberos
+Version:     1.0 BETA
+
+Dispositivo: PIC 16F886      
+Entorno IDE: MPLAB IDE v8.92
+Entorno IDE: PIC C Compiler
+Compilador:  CCS
+Simulador:   ISIS 7
+------------------------------------------------------------------------------------------------------
+NOTAS:
+-Se cuenta con dos sensores de fuego que determinar con precisiòn (pero con inestabilidad ambiental),
+la posiciòn del fuego. Estos se ajustan dependiendo de la distancia en que se quiere apagar el fuego.
+
+-Un sensor serà giratorio (muy sensible) y otro estarà adelante del dispositivo (se calibro muy justo)
+------------------------------------------------------------------------------------------------------
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Linea 0///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include    <16F886.h>
+#use        delay(clock = 8M)
+
+#use fast_io(a)
+#use fast_io(b)
+#use fast_io(c)
+
+#fuses      INTRC_IO
+#fuses      NOWDT
+#fuses      NOMCLR
+
+//-----------Entradas-------------//
+#DEFINE  sensor_frontal    PIN_A0 //Sensor detector de proximidad al fuego
+#DEFINE  sensor_giratorio  PIN_B0 //Sensor detector de aparicion de fuego
+#DEFINE  fd_carrera        PIN_E3 //Final de carrera para seteo del Brandbil
+//--------------------------------//
+
+//-----------Salidas--------------//
+#DEFINE  water_pump        PIN_B1 //Bomba de agua
+                                  //
+#DEFINE  pap_in1           PIN_B5 //Entrada IN1 del paso a paso
+#DEFINE  pap_in2           PIN_B4 //Entrada IN2 del paso a paso
+#DEFINE  pap_in3           PIN_B3 //Entrada IN3 del paso a paso
+#DEFINE  pap_in4           PIN_B2 //Entrada IN4 del paso a paso
+                                  //
+#DEFINE  timer555_reset    PIN_C1 //Activa o no la alarma (reset del 555)
+                                  //
+#DEFINE  inputh1_1         PIN_C2 //Entrada del puente H (Puente 1 entrada 1)
+#DEFINE  inputh1_2         PIN_C3 //Entrada del puente H (Puente 1 entrada 2)
+#DEFINE  inputh2_1         PIN_C4 //Entrada del puente H (Puente 2 entrada 1)
+#DEFINE  inputh2_2         PIN_C5 //Entrada del puente H (Puente 2 entrada 2)
+//--------------------------------//
+
+//-----Variables o constantes-----//
+const int8  tiempopap = 10;               //Tiempo en milisegundos   
+const int16 paptiempo_vuelta = 20;        //Tiempo en segundos
+const int16 brandtiempo_mediavuelta = 2;
+/*
+const int16 brandtiempo_mediavuelta_derecha   = 4;  //Tiempo en segundos
+const int16 brandtiempo_mediavuelta_izquierda = 4;  //Tiempo en segundos
+*/
+const float tiempopap_calculo = 0.04;     //Tiempo en segundos
+      int   flag_rb0 = 0;                 //Se utiliza para saber si se dio la
+                                          //interrupciòn por RB0.
+      int16 n_veces_pap = 0;              //Contador para el paso a paso
+      int16 n_veces_brandbil= 0;          //Contador para el Brandbil
+      float grado_actual = 0;             //Posiciòn actual del paso a paso
+      float grados_rotacion;              //Posiciòn actual del paso a paso
+      float tiempo_rotacion;              //Tiempo en que tiene que girar el
+                                          //Brandbil para dirigirse al fuego
+                                         
+                                          //Datos para el PWM para corregir el
+                                          //movimiento de los motores ya que
+                                          //si no se usa PWM el dispositivo se
+                                          //va de manera irregular
+                                          //Los valores se calibraron
+                                          
+const int16 pwm_activoh1_girar =   1900;
+const int16 pwm_noactivoh1_girar = 100;
+ 
+const int16 pwm_activoh2_girar =   1700;
+const int16 pwm_noactivoh2_girar = 300; 
+
+const int16 pwm_activoh1_adelante = 1450;
+const int16 pwm_noactivoh1_adelante =550;
+
+const int16 pwm_activoh2_adelante = 1550;
+const int16 pwm_noactivoh2_adelante =450;
+
+const int16 pwm_activoh1_atras = 1500;
+const int16 pwm_noactivoh1_atras =500;
+
+const int16 pwm_activoh2_atras = 1800;
+const int16 pwm_noactivoh2_atras = 200;
+
+//-----INSTRUCCIONES PROPIAS-----//
+
+#DEFINE setear_grados   grado_actual = ( ( n_veces_pap * tiempopap_calculo * 360 ) / paptiempo_vuelta )
+#DEFINE calcular_tiempo tiempo_rotacion = (grados_rotacion * brandtiempo_mediavuelta) / 180
+
+//-------------------------------//
+#INT_EXT
+void sensorgiratorio_activado(){
+flag_rb0 = 1;
+}
+
+void mover_papizq(){
+
+#asm
+
+BCF 06,05
+BCF 06,04
+BSF 06,03
+BSF 06,02
+
+#endasm
+
+delay_ms(tiempopap);
+
+#asm
+
+BSF 06,05
+BCF 06,04
+BCF 06,03
+BSF 06,02
+
+#endasm
+
+delay_ms(tiempopap);
+
+#asm
+
+BSF 06,05
+BSF 06,04
+BCF 06,03
+BCF 06,02
+
+#endasm
+
+delay_ms(tiempopap);
+
+#asm
+
+BCF 06,05
+BSF 06,04
+BSF 06,03
+BCF 06,02
+
+#endasm
+
+delay_ms(tiempopap);
+
+}
+void mover_papder(){
+
+#asm
+
+BCF 06,05
+BCF 06,04
+BSF 06,03
+BSF 06,02
+
+#endasm
+
+delay_ms(tiempopap);
+
+#asm
+
+BCF 06,05
+BSF 06,04
+BSF 06,03
+BCF 06,02
+
+#endasm
+
+delay_ms(tiempopap);
+
+#asm
+
+BSF 06,05
+BSF 06,04
+BCF 06,03
+BCF 06,02
+
+#endasm
+
+delay_ms(tiempopap);
+
+#asm
+
+BSF 06,05
+BCF 06,04
+BCF 06,03
+BSF 06,02
+
+#endasm
+
+delay_ms(tiempopap);
+
+}
+void mover_brandbil_adelante(){
+
+#asm
+BCF    07.2
+BSF    07.3
+BSF    07.4
+BCF    07.5
+#endasm
+
+delay_us(pwm_activoh1_adelante);
+
+#asm
+BCF    07.2
+BCF    07.3
+#endasm
+
+delay_us(pwm_activoh2_adelante-pwm_activoh1_adelante);
+
+#asm
+BCF    07.2
+BCF    07.3
+BCF    07.4
+BCF    07.5
+#endasm
+
+delay_us(pwm_noactivoh2_adelante);
+
+}
+void mover_brandbil_atras(){
+#asm
+BSF    07.2
+BCF    07.3
+BCF    07.4
+BSF    07.5
+#endasm
+
+delay_us(pwm_activoh1_atras);
+
+#asm
+BCF    07.2
+BCF    07.3
+#endasm
+
+delay_us(pwm_activoh2_atras-pwm_activoh1_atras);
+
+#asm
+BCF    07.2
+BCF    07.3
+BCF    07.4
+BCF    07.5
+#endasm
+
+delay_us(pwm_noactivoh2_atras);
+
+}
+void frenar_brandbil(){
+#asm
+BCF    07.2
+BCF    07.3
+BCF    07.4
+BCF    07.5
+#endasm
+}
+void girar_brandbil_derecha(){
+
+#asm
+BCF    07.2
+BSF    07.3
+BCF    07.4
+BSF    07.5
+#endasm
+
+delay_us(pwm_activoh1_girar);
+
+#asm
+BCF    07.2
+BCF    07.3
+#endasm
+
+delay_us(pwm_activoh2_girar-pwm_activoh1_girar);
+
+#asm
+BCF    07.2
+BCF    07.3
+BCF    07.4
+BCF    07.5
+#endasm
+
+delay_us(pwm_noactivoh2_girar);
+
+}
+void girar_brandbil_izquierda(){
+
+#asm
+BSF    07.2
+BCF    07.3
+BSF    07.4
+BCF    07.5
+#endasm
+
+delay_us(pwm_activoh1_girar);
+
+#asm
+BCF    07.2
+BCF    07.3
+#endasm
+
+delay_us(pwm_activoh2_girar-pwm_activoh1_girar);
+
+#asm
+BCF    07.2
+BCF    07.3
+BCF    07.4
+BCF    07.5
+#endasm
+
+delay_us(pwm_noactivoh2_girar);
+
+}
+
+void main(){
+
+setup_oscillator(OSC_8MHZ);       //Se usa oscilador interno a una frecuencia de 8MHz
+
+set_tris_a(0b00000001);           //Se configuran las I/O de cada puerto
+set_tris_b(0b00000001);
+set_tris_c(0b00000000);
+set_tris_e(0b00001000);
+
+//-----------------------------------//
+//Configuraciòn de las interrupciones//
+//-----------------------------------//
+ext_int_edge(H_TO_L);
+
+#asm
+BCF   03.5
+MOVLW 144
+MOVWF 11
+BCF   11,7
+#endasm
+
+//-----------------------------------//
+//      Calibraciòn del equipo       //
+//-----------------------------------//
+//Seteo de los dispositivos en el reposo
+output_low(timer555_reset);
+frenar_brandbil();
+ 
+output_high(water_pump);
+delay_ms(1000);
+output_low(water_pump);
+
+//Seteo del grado de refencia
+while( input(fd_carrera) )
+   mover_papder();
+
+for(; n_veces_pap != 250 ; n_veces_pap++)
+   mover_papder();
+
+n_veces_pap = 0;
+
+#asm
+BSF   11,7
+#endasm
+
+while(1){
+
+//Se va guardando la posiciòn en que se encuentra el paso a paso, asi luego
+//saber en que posiciòn se encuentra el fuego
+
+//Gira para la izquierda hasta dar una vuelta
+for(; n_veces_pap != 515 && !(flag_rb0 == 1) ; n_veces_pap++)
+{
+mover_papizq();
+setear_grados;
+}
+
+//Gira para la derecha hasta dar una vuelta
+for(; n_veces_pap != 0   && !(flag_rb0 == 1) ; n_veces_pap--)
+{
+mover_papder();
+setear_grados;
+}
+
+//En el caso que se de la interrupcion, el programa harà la funcion de este if
+//que es acercarce al fuego, y apagarlo
+//Lo podemos considerar como el programa principal
+
+if(flag_rb0 == 1){
+
+//Se tiene que saber cuanto tiempo tiene que girar para ir hacia el fuego 
+//entonces se pregunta en que grados paro el sensor.
+//En el caso en que el grado_actual sea menor a 180º, girar para la derecha.
+//Si es mayor, girara a la izquierda.
+//Asi se ahorra consumo electrico.
+
+if(grado_actual <= 180){
+
+grados_rotacion = grado_actual;
+calcular_tiempo;
+
+for(n_veces_brandbil = 0; n_veces_brandbil <= (tiempo_rotacion * 500)  ; n_veces_brandbil++ ){
+girar_brandbil_izquierda();
+}
+
+}
+else if(grado_actual >= 180){
+
+grados_rotacion = 360 - grado_actual;
+calcular_tiempo;
+
+for(n_veces_brandbil = 0; n_veces_brandbil <= (tiempo_rotacion * 500)  ; n_veces_brandbil++ ){
+girar_brandbil_derecha();
+}
+}
+
+//Se activa la alarma una vez girado
+output_high(timer555_reset);
+delay_ms(1000);
+
+//Se mueve el brandbil hasta que llega al fuego
+while(input(sensor_frontal))
+mover_brandbil_adelante();
+
+frenar_brandbil();         //Frenamos el Brandbil
+delay_ms(1000);            //Se espera 1 seg.
+output_high(water_pump);   //Se activa la bomba de agua
+delay_ms(15000);           //Se vacia el tanque
+output_low(water_pump);    //Se desactiva la bomba de agua
+delay_ms(2000);            //Se espera 2 seg.
+output_low(timer555_reset);//Se desactriva la alarma
+flag_rb0 = 0;              //Se baja el flag para dar por finalizado haber
+                           //apagado el fuego.
+
+}
+
+}
+
+
+
+
+}
